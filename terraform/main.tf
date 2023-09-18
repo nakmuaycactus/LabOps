e86c389c6192c0348fdbe765f49aaf6dc28b203d
Jesse O'Brien <74101028+jesseobrien247@users.noreply.github.com>
2023-09-18 15:18:20 +0800

    Added additional VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,8 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "terraUL" {
-  name             = "terraUL"
+resource "vsphere_virtual_machine" "ubuntuVM" {
+  name             = "ubuntuVM"
   datacenter_id    = data.vsphere_datacenter.dc.id
   datastore_id     = data.vsphere_datastore.datastore.id
   resource_pool_id = data.vsphere_resource_pool.pool.id
@@ -60,8 +60,83 @@ resource "vsphere_virtual_machine" "terraUL" {
 
   vapp {
     properties = {
-      "guestinfo.hostname" = "terraUL",
+      "guestinfo.hostname" = "ubuntuVM",
       #"guestinfo.ipaddress"    = "192.168.3.152",
+      #"guestinfo.netmask"      = "255.255.255.0",
+      #"guestinfo.gateway"      = "192.168.3.1",
+      #"guestinfo.dns"          = "8.8.8.8",
+      "guestinfo.domain"   = "example.com",
+      "guestinfo.ntp"      = "ntp.example.com",
+      "guestinfo.password" = "Thrunters1!FTW",
+      "guestinfo.ssh"      = "True"
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "kaliVM" {
+  name             = "kaliVM"
+  datacenter_id    = data.vsphere_datacenter.dc.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  host_system_id   = data.vsphere_host.host.id
+
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout  = 0
+
+  ovf_deploy {
+    allow_unverified_ssl_cert = true
+    local_ovf_path            = "../output/KL/packerKL.ovf"
+    disk_provisioning         = "thin"
+    ip_protocol               = "IPV4"
+    ip_allocation_policy      = "STATIC_MANUAL"
+    ovf_network_map = {
+      "Network 1" = data.vsphere_network.network.id
+      "Network 2" = data.vsphere_network.network.id
+    }
+  }
+
+  vapp {
+    properties = {
+      "guestinfo.hostname" = "kaliVM",
+      #"guestinfo.ipaddress"    = "192.168.3.153",
+      #"guestinfo.netmask"      = "255.255.255.0",
+      #"guestinfo.gateway"      = "192.168.3.1",
+      #"guestinfo.dns"          = "8.8.8.8",
+      "guestinfo.domain"   = "example.com",
+      "guestinfo.ntp"      = "ntp.example.com",
+      "guestinfo.password" = "Thrunters1!FTW",
+      "guestinfo.ssh"      = "True"
+    }
+  }
+}
+
+
+resource "vsphere_virtual_machine" "rockyVM" {
+  name             = "rockyVM"
+  datacenter_id    = data.vsphere_datacenter.dc.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  host_system_id   = data.vsphere_host.host.id
+
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout  = 0
+
+  ovf_deploy {
+    allow_unverified_ssl_cert = true
+    local_ovf_path            = "../output/RL/packerRL.ovf"
+    disk_provisioning         = "thin"
+    ip_protocol               = "IPV4"
+    ip_allocation_policy      = "STATIC_MANUAL"
+    ovf_network_map = {
+      "Network 1" = data.vsphere_network.network.id
+      "Network 2" = data.vsphere_network.network.id
+    }
+  }
+
+  vapp {
+    properties = {
+      "guestinfo.hostname" = "rockyVM",
+      #"guestinfo.ipaddress"    = "192.168.3.154",
       #"guestinfo.netmask"      = "255.255.255.0",
       #"guestinfo.gateway"      = "192.168.3.1",
       #"guestinfo.dns"          = "8.8.8.8",

cc89330328734b8aca50a04e538f2784b3707bcd
Jesse O'Brien <74101028+jesseobrien247@users.noreply.github.com>
2023-09-20 16:41:14 +0800

    Updated ip

--- packer2/http/ser-16-setup/setup.ps1
+++ packer2/http/ser-16-setup/setup.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass
 
-$IP = "192.168.3.151"
+$IP = "192.168.3.154"
 $MaskBits = 24 # This means subnet mask = 255.255.255.0
 $Gateway = "192.168.3.1"
 $Dns = "1.1.1.1"

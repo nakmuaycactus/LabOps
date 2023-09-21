245e2469b389ee4e7f9c9874a5532d40d5b0c531
Jesse O'Brien <74101028+jesseobrien247@users.noreply.github.com>
2023-09-21 14:46:27 +0800

    Disabled broken build

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ packer build -force -var-file=flavours/KL-pkrvars.hcl linuxTemplate.pkr.hcl
 packer init windowsTemplate.pkr.hcl
 packer build -force -var-file=flavours/ser-22.pkrvars.hcl windowsTemplate.pkr.hcl
 packer build -force -var-file=flavours/win-10.pkrvars.hcl windowsTemplate.pkr.hcl
-packer build -force -var-file=flavours/ser-16.pkrvars.hcl windowsTemplate.pkr.hcl
+#packer build -force -var-file=flavours/ser-16.pkrvars.hcl windowsTemplate.pkr.hcl

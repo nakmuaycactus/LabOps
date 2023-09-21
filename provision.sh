0557490f6e27bf6337d6421995602c958cb7f774
Jesse O'Brien <74101028+jesseobrien247@users.noreply.github.com>
2023-09-21 14:42:40 +0800

    Added required ansible-galaxy community collections

--- provision.sh
+++ provision.sh
@@ -9,6 +9,12 @@ ssh-keygen -R 192.168.3.151
 ssh-keygen -R 192.168.3.152
 
 cd ../ansible
+
+ansible-galaxy collection install community.general
+ansible-galaxy collection install community.vmware
+ansible-galaxy collection install community.mysql
+
+
 ansible-playbook -i=hosts esxi.yml
 sleep 1m
 ansible-playbook -i=hosts ubuntu.yml rocky.yml
